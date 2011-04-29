753318796ce459156993254a639560cade323ced
Anita Kuno <ay.kuno@gmail.com>
2011-04-28 20:59:07 -0400

    I think this satisfies the requirements of the second step number 1

---
+++ test/list_test.rb
@@ -0,0 +1,13 @@
+require File.join(File.dirname(__FILE__), '..', 'list')
+require 'test/unit'
+
+class TestList < Test::Unit::TestCase
+  def test_initialize
+  end
+
+  def test_first_node
+  end
+
+  def test_unlink_next
+  end
+end
---
+++ test/node_test.rb
@@ -0,0 +1,7 @@
+require File.join(File.dirname(__FILE__), '..', 'node')
+require 'test/unit'
+
+class TestNode < Test::Unit::TestCase
+  def test_insert_after_me
+  end
+end
