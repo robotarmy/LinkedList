6e1d28c2ca96125bd77f18ae9fe52a868b23bbef
Anita Kuno <ay.kuno@gmail.com>
2011-04-28 20:46:04 -0400

    completed step 0 and the first step number 1

---
+++ list.rb
@@ -0,0 +1,31 @@
+<<-LIST
+class List
+
+  def initialize(beginning_node)
+  end
+
+  #:record_attr
+  def first_node
+    # return begining of list or nil if empty
+  end
+
+  #
+  # function insertBeginning(List list, Node newNode) // insert node beforhave the value 'z'
+#    3) define a 'sort' method for the List objecste List will have a number (n > 0 < n)
+#       2) calling sort on the List object will return a new lisveList object will return a new list
+#       3) the original list wilt. #:record_attr
+#    1) design assertions according to comment about method - failing test
+#    2) writeNethod - failing test
+#    2) write code - passing test
+#
+# 3) for ese#    1) design assertions according to comment about method - failing test
+#    2) write code - passinde.next
+    #   node.next := node.next.next
+    #   destroy obsoleteNode
+    #   
+    def unlink_next(node)
+      #return node
+    end
+  end
+end
+LIST
